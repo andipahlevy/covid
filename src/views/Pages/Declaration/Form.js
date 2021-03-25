8b4dfb7031bf2dfea84462bbf1898ebe8a9dbdce
Andi Levi <andilevi@gmail.com>
2021-03-25 13:39:02 +0700

    f

--- src/views/Pages/Declaration/Form.js
+++ src/views/Pages/Declaration/Form.js
@@ -229,19 +229,21 @@ class Declare extends Component {
 									<img width="50px" src="/assets/img/logo.png" alt="" className="image"/>
 								</div>
 								<div className="col-xs-12 col-sm-12 text-center">
-									<h1 style={{marginTop: '10px'}}>PT. Triputra Agro Persada Tbk</h1>
+									<h1 style={{marginTop: '10px'}}>PT. Triputra Agro Persada, Tbk</h1>
 								</div>
 							</div>
 							<div className="row d-none d-sm-block">
 								<div className="row row p-3">
-									<div className="col-md-9">
-										<h1 style={{marginTop: '10px'}}>PT. Triputra Agro Persada Tbk</h1>
+									<div className="col-md-1" style={{ textAlign: 'right' }}>
+										<img width="50px" src="/assets/img/logo.png" alt="" className="image"/>
 									</div>
-									<div className="col-md-3" style={{ textAlign: 'right' }}><img width="50px" src="/assets/img/logo.png" alt="" className="image"/></div>
+									<div className="col-md-11">
+										<h1 style={{marginTop: '10px'}}>PT. Triputra Agro Persada, Tbk</h1>
+									</div>
 								</div>
 							</div>
 							<h2 style={{ color:'#5c6873' }}>Semangat Pagi Insan TAP</h2>
-							<h4 style={{ color:'#5c6873' }}>Self Declaration</h4>
+							<h4 style={{ color:'#5c6873' }}>Self Assessment</h4>
 							<br/>
 							<p className="text-muted">Di tengah merebaknya virus COVID19 (Novel Coronavirus), maka manajemen perlu mengetahui lebih lanjut kondisi dari setiap karyawan HO (Head Office) yang memiliki keluhan ataupun masalah kesehatan pada hari ini atau dalam 1 minggu terakhir ini.
 		Langkah ini dilakukan sebagai  wujud kepedulian serta tindakan pencegahan dan solusi agar seluruh karyawan TAP Group selalu dalam keadaan sehat dan tetap dapat beraktivitas seperti biasa.</p>
